595e98d133db64e294241cc1a8067c0f58a8f8b3
timvernaeve <r0754975@student.thomasmore.be>
2022-11-16 17:21:41 +0100

    feat(Messages): add basic template structure for comments

---
+++ src/components/Messages.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+         <ul>
+            <li v-for="message in messages" :key="message.id">
+                <h3>{{ message.username }}</h3>
+                <p>{{ message.message }}</p>
+            </li>
+        </ul>    
+    </div>
+    <div>
+        <input v-model="comment">
+        <button @click="addComment">Add Comment</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
